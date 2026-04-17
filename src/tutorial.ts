350c7b8d3231fe4cee8003c82db8b6c38fb05e8a
nanda <nandakishorelogu@gmail.com>
2026-04-17 23:08:13 +0530

    completed till arrays

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -72,3 +72,30 @@ discount = "10%";
 // discount = false;
 
 console.log(discount);
+
+// arrays in typescript
+
+let prices: number[] = [10, 20, 30];
+
+let fruit: string[] = ["apple", "orange"];
+
+// let emptyArr: [] = [1]; //  "[]" means literally empty array it will take no elements so it will declare as empty array only
+// fix
+// let emptyArr: number[] = [20];
+// let emptyArr: number[] = [];
+
+// union type in array - take multiple data types
+
+let takeNumandString: (number | string)[] = [10, 20, "Apple", "Mango"];
+
+console.log(takeNumandString);
+
+// challenge topic in  array
+let temparature: number[] = [100, 90, 80];
+// temparature.push("cool") // this will result in typeError
+
+let colors: string[] = ["red", "yellow", "green"];
+//colors.push(180); // this will result in typeError
+
+let user: (string | number)[] = ["John", "peter", 100, 90];
+// usrt.push(true); // this array will take only string and number data type values not boolean which will result in typeError
